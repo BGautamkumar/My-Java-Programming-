417c03796e10636838f662df4205f138b8fa3daa
Gautam Kumar Budida <157948344+BGautamkumar@users.noreply.github.com>
2024-02-26 13:47:52 +0530

    Add files via upload

---
+++ fibonacci using recursion.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class javaRecursions {
+   //Calculate the Nth term in fibonacci number
+    public static int fib(int n) {
+        if(n == 0 || n == 1) {
+            return n;
+        }
+        int fnm1 = fib(n-1);
+        int fnm2 = fib(n-2);
+        int fn = fnm1 + fnm2;  
+        return fn;
+    }
+    public static void main(String args[]) {
+        int n = 26;
+        System.out.println(fib(n));
+    }
+}
